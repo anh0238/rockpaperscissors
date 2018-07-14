61f9877e12fc1787f0104d07c76fa159ed6d491b
ahend <anh0238@gmail.com>
2018-07-14 14:07:00 -0500

    class files

---
+++ rps/scritp.js
@@ -0,0 +1,34 @@
+//First, print instructions on screen,
+
+//Then, declare you are playing the game (state variables, state how to start)
+var wins = 0
+var losses = 0
+var ties = 0
+
+var computerMove = ["r", "p", "s"];
+
+function getRandomArbitrary(_computerMove) {
+    return ("")
+}
+
+
+//Math.floor(Math.random() * Math.floor(max));
+//console.log(getRandomInt(3));
+//expected output 0, 1, or 2
+
+
+
+//State how to win, rules and conditions
+
+//On 'key press' declare move, make this a variable
+
+//Have computer select a move at random, record this as a variable
+var r = 
+var p = 
+var s = 
+
+//Determine who has won
+
+//Record score (make sure it is a running score, does not reset)
+
+//Play again with a key press
